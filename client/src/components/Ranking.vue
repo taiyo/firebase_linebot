5fbcea721b22a57a7c9cd8b11ff97168588d8de6
taiyo <taiyo.kato.1209@gmail.com>
2018-05-30 21:42:28 +0900

    投稿情報のサマリを表示するページを作成 #5

---
+++ client/src/components/Ranking.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-table :data="ranks" :columns="columns"></b-table>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      columns: [
+        { field: 'rank', label: 'ランキング' },
+        { field: 'profile.displayName', label: '名前' },
+        { field: 'count', label: '投稿数' }
+      ],
+      ranks: []
+    }
+  },
+  created () {
+    const self = this
+    Axios.get(process.env.RANK_URL).then(res => {
+      self.ranks = res.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>

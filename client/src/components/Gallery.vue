e2b229083d6c96e875089953b522a0ea7c322793
taiyo <taiyo.kato.1209@gmail.com>
2018-05-30 22:17:49 +0900

    投稿画像ギャラリを実装（ライブラリお試し） #3

--- client/src/components/Gallery.vue
+++ client/src/components/Gallery.vue
@@ -1,32 +1,46 @@
 <template>
-  <vue-picture-swipe :items="items"></vue-picture-swipe>
+  <div>
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
+    <div
+      class="image"
+      v-for="(image, imageIndex) in images"
+      :key="imageIndex"
+      @click="index = imageIndex"
+      :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
+    ></div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import VuePictureSwipe from 'vue-picture-swipe' // eslint-disable-line
 
-Vue.component('vue-picture-swipe', VuePictureSwipe)
+import VueGallery from 'vue-gallery' // eslint-disable-line
 
 export default {
-  name: 'gallery',
-  data () {
+  data: function () {
     return {
-      items: [
-        {
-          src: 'http://via.placeholder.com/600x400',
-          thumbnail: 'http://via.placeholder.com/64x64',
-          w: 600,
-          h: 400
-        },
-        {
-          src: 'http://via.placeholder.com/1200x900',
-          thumbnail: 'http://via.placeholder.com/64x64',
-          w: 1200,
-          h: 900
-        }
-      ]
+      images: [
+        'https://dummyimage.com/800/ffffff/000000',
+        'https://dummyimage.com/1600/ffffff/000000',
+        'https://dummyimage.com/1280/000000/ffffff',
+        'https://dummyimage.com/400/000000/ffffff'
+      ],
+      index: null
     }
+  },
+
+  components: {
+    gallery: VueGallery
   }
 }
 </script>
+
+<style scoped>
+.image {
+  float: left;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  border: 1px solid #ebebeb;
+  margin: 5px;
+}
+</style>
